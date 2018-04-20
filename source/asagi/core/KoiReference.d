3b4e10428770aaab0b56f478e73f6338ba7b161b
DeflatedPickle <DeflatedPickle@gmail.com>
2018-04-20 13:51:39 +0100

    Added a reference class

---
+++ source/asagi/core/KoiReference.d
@@ -0,0 +1,23 @@
+module asagi.core.KoiReference;
+
+import std.stdio;
+
+import asagi.core.KoiObject;
+import asagi.core.types.KoiBoolean;
+import asagi.core.types.KoiCharacter;
+import asagi.core.types.KoiInteger;
+import asagi.core.types.KoiString;
+
+abstract class KoiReference : KoiObject {
+    override KoiBoolean asBoolean() {
+        return new KoiBoolean(true);
+    }
+
+    override KoiCharacter asCharacter() {
+        return new KoiCharacter(' ');
+    }
+
+    override KoiInteger asInteger() {
+        return new KoiInteger(0);
+    }
+
